6bf7b37d3128c742047bfd1a4dc5a33c5a929d8f
David Srour <david.srour@conffx.com>
2021-03-08 17:22:12 -0800

    - working on optimizing main loop

--- CoinGecko-Requests.py
+++ CoinGecko-Requests.py
@@ -10,13 +10,14 @@ import time
 
 # usd pairs from GDAX, Kraken 
 # + some extras
-filter = ['btc', 'eth', 'ltc', 'bch', 'eos', 'dash', 'oxt', 'mkr', 'xlm', 'atom', 'xtz', 'etc', 'omg', 'zec', 'link', 'rep', 'zrx', 'algo', 'dai', 'knc', 'comp', 'band', 'nmr', 'cgld', 'uma', 'lrc', 'yfi', 'uni', 'ren', 'bal', 'wbtc', 'nu', 'fil', 'aave', 'grt', 'bnt', 'snx',
-          'ant', 'bat', 'ada', 'crv', 'mana', 'xdg', 'ewt', 'flow', 'gno', 'icx', 'kava', 'keep', 'ksm', 'lsk', 'mln', 'xmr', 'nano', 'ocean', 'paxg', 'dot', 'qtum', 'xrp', 'sc', 'storj', 'trx', 'waves',
+filter = ['btc', 'eth', 'ltc', 'bch', 'eos', 'dash', 'oxt', 'mkr', 'xlm', 'atom', 'xtz', 'etc', 'omg', 'zec', 'link', 'rep', 'zrx', 'algo', 'dai', 'knc', 'comp', 'band', 'nmr', 'uma', 'lrc', 'yfi', 'uni', 'ren', 'bal', 'wbtc', 'nu', 'fil', 'aave', 'grt', 'bnt', 'snx',
+          'ant', 'bat', 'ada', 'crv', 'mana', 'doge', 'ewt', 'flow', 'gno', 'icx', 'kava', 'keep', 'ksm', 'lsk', 'xmr', 'nano', 'ocean', 'paxg', 'dot', 'qtum', 'xrp', 'sc', 'storj', 'trx', 'waves',
           'req']
 
 cg = CoinGeckoAPI()
 
-marketCaps = cg.get_coins_markets(vs_currency='usd', order="market_cap_desc", per_page="300")
+marketCaps = cg.get_coins_markets(vs_currency='usd', order="market_cap_desc", per_page=250, page=1)
+marketCaps += cg.get_coins_markets(vs_currency='usd', order="market_cap_desc", per_page=250, page=2)
 marketCapsJson = (json.dumps(marketCaps, indent=4, sort_keys=True))
 with open('MarketCaps.json', 'w') as f:
     json.dump(marketCaps, f)
@@ -31,37 +32,39 @@ avgAccelPerID = {} #avg mcap vel per increment
 curIndex = 0
 
 #go through each symbol
-while curIndex < len(marketCapDF["id"]):
+for ticker in filter:
     found = True
     try:
-        filter.index(marketCapDF["symbol"][curIndex])
-    except ValueError:
+        curIndex = marketCapDF["symbol"][marketCapDF["symbol"] == ticker].index[0]
+    except:
+        print('could not find ticker ' + ticker)
         found = False
     if  found:
-        if curIndex > 0 and curIndex % 9:
-                time.sleep(1.05) # so we don't get denied from too many queries
-
+        print('found ticker ' + ticker + " index = " + str(curIndex))
+        #if curIndex > 0 and curIndex % 9:
+        #        time.sleep(1.05) # so we don't get denied from too many queries
+        
         #get market chart which returns prices,market_caps & total_volumes.
-        market_chart = cg.get_coin_market_chart_by_id(id=marketCapDF["id"][curIndex], vs_currency='usd', days=30)
+        #market_chart = cg.get_coin_market_chart_by_id(id=marketCapDF["id"][curIndex], vs_currency='usd', days=30)
     
-        avgVel = []
-        avgAccel = []
-        avgAccel.append(0.00)
+        #avgVel = []
+        #avgAccel = []
+        #avgAccel.append(0.00)
 
-        #collect
-        i = 1
-        while i < len(market_chart["market_caps"]):
-            #need to divide by 1000 for some reason. Found a stack overflow solution.
-            #scatterData["data"][0]["x"].append(datetime.fromtimestamp(entry[0] / 1000))
-            #scatterData["data"][0]["y"].append(entry[1])
-            avgVel.append(market_chart["market_caps"][i][1] - market_chart["market_caps"][i-1][1])
-            if i > 1:
-                avgAccel.append(avgVel[i-1] - avgVel[i-2])
-            i += 1
+        ##collect
+        #i = 1
+        #while i < len(market_chart["market_caps"]):
+        #    #need to divide by 1000 for some reason. Found a stack overflow solution.
+        #    #scatterData["data"][0]["x"].append(datetime.fromtimestamp(entry[0] / 1000))
+        #    #scatterData["data"][0]["y"].append(entry[1])
+        #    avgVel.append(market_chart["market_caps"][i][1] - market_chart["market_caps"][i-1][1])
+        #    if i > 1:
+        #        avgAccel.append(avgVel[i-1] - avgVel[i-2])
+        #    i += 1
 
-        avgVelPerID[marketCapDF["symbol"][curIndex]] = avgVel
-        avgAccelPerID[marketCapDF["symbol"][curIndex]] = avgAccel
-
+        #avgVelPerID[marketCapDF["symbol"][curIndex]] = avgVel
+        #avgAccelPerID[marketCapDF["symbol"][curIndex]] = avgAccel
+        
     curIndex +=1
 
 print('done')
